baaa699649ab7fe4fe614c2f477c80ef7d44f67f
SengSerany <cranyseng@gmail.com>
2022-05-30 11:13:59 +0200

    create basic routes for dish

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ entryPoint: 'login' });
-});
+// Call routers
+const dishRouter = require('./routes/dishRoutes');
+
+// Set routes
+app.use('/api/v1/dish', dishRouter);
 
 app.listen(port, () => {
   console.log(`> Server is listening on port ${port}`);
