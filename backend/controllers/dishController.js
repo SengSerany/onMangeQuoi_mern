9a3ba336e785cc649c8e50f27f7eb7d402819a09
SengSerany <cranyseng@gmail.com>
2022-05-30 11:35:57 +0200

    use express-async-handler for asyn errors

--- backend/controllers/dishController.js
+++ backend/controllers/dishController.js
@@ -1,41 +1,43 @@
+const asyncHandler = require('express-async-handler');
+
 // Index
-const indexDishes = (req, res) => {
+const indexDishes = asyncHandler(async (req, res) => {
   res.status(200).json({ entre_point: 'Index dishes' });
-};
+});
 
 // Show
-const showDish = (req, res) => {
+const showDish = asyncHandler(async (req, res) => {
   res.status(200).json({ entre_point: 'Show dish' });
-};
+});
 
 // New
-const newDish = (req, res) => {
+const newDish = asyncHandler(async (req, res) => {
   res.status(200).json({ entre_point: 'New dish' });
-};
+});
 
 // Create
-const createDish = (req, res) => {
+const createDish = asyncHandler(async (req, res) => {
   if (!req.body.dish) {
     res.status(400);
     throw new Error('All field must be completed');
   }
   res.status(200).json({ entre_point: 'Create dish' });
-};
+});
 
 // Edit
-const editDish = (req, res) => {
+const editDish = asyncHandler(async (req, res) => {
   res.status(200).json({ entre_point: 'Edit dish' });
-};
+});
 
 // Update
-const updateDish = (req, res) => {
+const updateDish = asyncHandler(async (req, res) => {
   res.status(200).json({ entre_point: 'Update dish' });
-};
+});
 
 // Delete
-const deleteDish = (req, res) => {
+const deleteDish = asyncHandler(async (req, res) => {
   res.status(200).json({ entre_point: 'Delete dish' });
-};
+});
 
 module.exports = {
   indexDishes,
