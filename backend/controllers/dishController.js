88d47d35ab937e62e6abbf75eabc13c8577676a5
SengSerany <cranyseng@gmail.com>
2022-05-30 20:53:39 +0200

    user can only interact with his own dishes

--- backend/controllers/dishController.js
+++ backend/controllers/dishController.js
@@ -3,14 +3,23 @@ const Dish = require('../models/dishModel');
 
 // Index
 const indexDishes = asyncHandler(async (req, res) => {
-  const dishes = await Dish.find();
-  res.status(200).json({ entre_point: 'Index dishes', dishes });
+  const isDishAuth = req.query.dish;
+  const dishes = await Dish.find({ authID: req.user._id });
+  res.status(200).json({
+    entre_point: 'Index dishes',
+    auth: isDishAuth && "Couldn't found this dish",
+    dishes,
+  });
 });
 
 // Show
 const showDish = asyncHandler(async (req, res) => {
   const currentDish = await Dish.findById(req.params.id);
-  res.status(200).json({ entre_point: 'Show dish', currentDish });
+  if (currentDish.authID.toString() === req.user._id.toString()) {
+    res.status(200).json({ entre_point: 'Show dish', currentDish });
+  } else {
+    res.redirect('/api/v1/dish/?dish=false');
+  }
 });
 
 // New
@@ -31,27 +40,43 @@ const createDish = asyncHandler(async (req, res) => {
 // Edit
 const editDish = asyncHandler(async (req, res) => {
   const currentDish = await Dish.findById(req.params.id);
-  res.status(200).json({ entre_point: 'Edit dish', currentDish });
+  if (currentDish.authID.toString() === req.user._id.toString()) {
+    res.status(200).json({ entre_point: 'Edit dish', currentDish });
+  } else {
+    res.redirect('/api/v1/dish/?dish=false');
+  }
 });
 
 // Update
 const updateDish = asyncHandler(async (req, res) => {
   const currentDish = await Dish.findById(req.params.id);
-  const updatedDish = await Dish.findByIdAndUpdate(currentDish._id, req.body, {
-    new: true,
-  });
-  res.status(200).json({ entre_point: 'Update dish', updatedDish });
+  if (currentDish.authID.toString() === req.user._id.toString()) {
+    const updatedDish = await Dish.findByIdAndUpdate(
+      currentDish._id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({ entre_point: 'Update dish', updatedDish });
+  } else {
+    res.redirect('/api/v1/dish/?dish=false');
+  }
 });
 
 // Delete
 const deleteDish = asyncHandler(async (req, res) => {
   const currentDish = await Dish.findById(req.params.id);
-  currentDish.remove();
-  res.status(200).json({
-    entre_point: 'Delete dish',
-    id: currentDish._id,
-    name: currentDish.name,
-  });
+  if (currentDish.authID.toString() === req.user._id.toString()) {
+    currentDish.remove();
+    res.status(200).json({
+      entre_point: 'Delete dish',
+      id: currentDish._id,
+      name: currentDish.name,
+    });
+  } else {
+    res.redirect('/api/v1/dish/?dish=false');
+  }
 });
 
 module.exports = {
