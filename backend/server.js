5300570d8cc7d8dc76230a855b1bf4665d5e191f
SengSerany <cranyseng@gmail.com>
2022-05-30 11:31:59 +0200

    create personal error handler

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const app = express();
 require('dotenv').config();
+const app = express();
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const port = process.env.PORT || 5000;
 
 // Call routers
 const dishRouter = require('./routes/dishRoutes');
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Set routes
 app.use('/api/v1/dish', dishRouter);
 
+// Error middleware
+app.use(errorHandler);
+
+// Listining
 app.listen(port, () => {
   console.log(`> Server is listening on port ${port}`);
 });
